bc8b18842e5436df504a599606075c1259a8c953
Zirpoo <loik77@live.ca>
2019-10-19 00:43:15 -0400

    Linux file uppercase rename

--- src/Services/dataAPI/index.js
+++ src/Services/dataAPI/index.js
@@ -16,7 +16,21 @@ export default function() {
 		const app = express();
 		app.use(express.json());
 		app.use(express.urlencoded({ extended: true }));
-
+		app.use((req, res, next) => {
+			res.setHeader(
+				"Access-Control-Allow-Origin",
+				"http://localhost:3000"
+			);
+			res.setHeader(
+				"Access-Control-Allow-Headers",
+				"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+			);
+			res.setHeader(
+				"Access-Control-Allow-Methods",
+				"GET, POST, PATCH, DELETE, OPTIONS"
+			);
+			next();
+		});
 		for (const filename of routes) {
 			const Route = require(path.join(routePath, filename));
 			Route.default(app);
